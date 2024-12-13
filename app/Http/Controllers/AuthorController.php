90973e51916a41e1cbea4c56255d61e434f516cb
Soulaimane Yahya <soulaimaneyh07@gmail.com>
2024-12-13 16:03:56 +0100

    fixing

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthorController extends Controller
@@ -18,6 +20,58 @@ class AuthorController extends Controller
     public function show(string $authorId): JsonResponse
     {
         $author = Author::findOrFail($authorId);
+
+        return $this->successResponse($author);
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:authors,email',
+            'country' => 'required|max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->validated());
+
+        return $this->successResponse($author, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $author)
+    {
+        $author = Author::findOrFail($author);
+
+        $rules = [
+            'name' => 'max:255',
+            'email' => 'email|unique:authors,email,' . $author->email,
+            'country' => 'max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author->fill($request->validated());
+
+        if ($author->isClean()) {
+            return $this->errorResponse(
+                'At least one value must change',
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $author->save();
+
+        return $this->successResponse($author);
+    }
+
+    public function destroy($author)
+    {
+        $author = Author::findOrFail($author);
+
+        if ($author instanceof Author) {
+            $author->delete();
+        }
 
         return $this->successResponse($author);
     }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-
 $router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
 $router->get('/authors/{authorId}', 'AuthorController@show');
+$router->put('/authors/{authorId}', 'AuthorController@update');
+$router->delete('/authors/{authorId}', 'AuthorController@destroy');
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
